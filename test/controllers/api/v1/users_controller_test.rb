a407f1f82c95c7ae859a2511b2e5046e1dcbd9df
Fernando Cendretti <luiz.cendretti@gamil.com>
2023-04-11 14:52:00 -0300

    refactor user test and add plan_fabricator

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -2,10 +2,21 @@ require 'test_helper'
 class Api::V1::UsersControllerTest < ActionController::TestCase
   def setup
     @user = Fabricate(:user)
+    @plan = Fabricate(:plan)
   end
+
   test 'should be able to create a new user' do
-    post :create, name: 'John', email: 'john@email.com', password: '123', password_confirmation: '123', plan_id: 1
+    body = {
+      name: 'Fernando',
+      email: 'fernando@email.com',
+      password: '123',
+      password_confirmation: '123'
+    }
+    post :create, body
     assert_response :success
-    User.find()
+    @json = JSON.parse(response.body)
+    assert_equal @json['name'], 'Fernando'
+    assert_equal @json['email'], 'fernando@email.com'
+    assert_not_empty @json['id']
   end
 end
--- test/fabricators/plan_fabricator.rb
+++ test/fabricators/plan_fabricator.rb
@@ -5,17 +5,3 @@ Fabricator(:plan) do
   limit_folder 10
   limit_notes 50
 end
-Fabricator(:plan) do
-  id { UUIDTools::UUID.random_create }
-  name 'Standart'
-  value 49.99
-  limit_folder 100
-  limit_notes 500
-end
-Fabricator(:plan) do
-  id { UUIDTools::UUID.random_create }
-  name 'Deluxe'
-  value 99.99
-  limit_folder 100000
-  limit_notes 500000
-end
